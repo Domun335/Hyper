8883c5080a8cb077f2874819e75d7c9460ecad18
Domun <zwolinskid1@gmail.com>
2023-07-19 18:28:29 +0200

    Update

---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <>
+      <h1>CMS Test</h1>
+    </>
+  )
+}
--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['media.graphassets.com'],
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
